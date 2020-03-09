80ab254989f3367e511c8af83156d2cdc6370b39
J. Ali <jali@Js-MacBook-Pro.local>
2020-03-09 01:16:52 -0400

    Test

--- index.php
+++ index.php
@@ -95,21 +95,21 @@
           <div class="mt-5">
             <i class="fas fa-4x fa-truck text-primary mb-4"></i>
             <h3 class="h4 mb-2">Office Moving</h3>
-            <p class="text-muted mb-0">Our themes are updated regularly to </p>
+            <p class="text-muted mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero vel.</p>
           </div>
         </div>
         <div class="col-lg-4 col-md-6 text-center">
           <div class="mt-5">
             <i class="fas fa-4x fa-tools text-primary mb-4"></i>
             <h3 class="h4 mb-2">Furniture Installation</h3>
-            <p class="text-muted mb-0">All dependencies are kept current to keep things fresh.</p>
+            <p class="text-muted mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero vel.</p>
           </div>
         </div>
         <div class="col-lg-4 col-md-6 text-center">
           <div class="mt-5">
             <i class="fas fa-4x fa-dumpster text-primary mb-4"></i>
             <h3 class="h4 mb-2">Furniture Disposal</h3>
-            <p class="text-muted mb-0">You can use this design as is, or you can make changes!</p>
+            <p class="text-muted mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero vel.</p>
           </div>
         </div>
 
@@ -181,7 +181,7 @@
   <section class="page-section bg-dark text-white">
     <div class="container text-center">
       <h2 class="mb-4">Get a Free Estimate Today!</h2>
-      <a class="btn btn-light btn-xl" href="https://startbootstrap.com/themes/creative/">Request an Estimate</a>
+      <a class="btn btn-light btn-xl" href="">Request an Estimate</a>
     </div>
   </section>
 
@@ -190,20 +190,20 @@
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-lg-8 text-center">
-          <h2 class="mt-0">Let's Get In Touch!</h2>
+          <h2 class="mt-0">Get In Touch!</h2>
           <hr class="divider my-4">
-          <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
+          <p class="text-muted mb-5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat enim eaque ab ipsum quis, assumenda qui impedit.</p>
         </div>
       </div>
       <div class="row">
         <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
-          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-          <div>+1 (202) 555-0149</div>
+          <i class="fas fa-phone fa-3x mb-3 text-muted" href="tel:6136081121"></i>
+          <a class="d-block" href="tel:6136081121">(613) 608 1121</a>
         </div>
         <div class="col-lg-4 mr-auto text-center">
           <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
           <!-- Make sure to change the email address in anchor text AND the link below! -->
-          <a class="d-block" href="mailto:contact@yourwebsite.com">contact@yourwebsite.com</a>
+          <a class="d-block" href="mailto:contact@capcityoffice.com">contact@capcityoffice.com</a>
         </div>
       </div>
     </div>
@@ -218,7 +218,7 @@
 
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
-  <!-- <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Plugin JavaScript -->
   <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
