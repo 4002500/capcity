57f77319259022777ef120b50f1c0a9c89cf61c3
J. Ali <jali@Js-MacBook-Pro.local>
2020-03-12 16:55:41 -0400

    Custom updates

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php $company_name = "CapCity Office Technitions" ?>
+<?php $email = "j624ali@gmail.com" ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -23,7 +24,7 @@
   <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
 
   <!-- Theme CSS - Includes Bootstrap -->
-  <link href="css/creative.min.css" rel="stylesheet">
+  <link href="css/creative.css" rel="stylesheet">
 
 </head>
 
@@ -48,7 +49,7 @@
             <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            <a class="nav-link js-scroll-trigger" data-toggle="modal" data-target="#modal" href="">Contact</a>
           </li>
         </ul>
       </div>
@@ -64,7 +65,10 @@
           <hr class="divider my-4">
         </div>
         <div class="col-lg-8 align-self-baseline">
-          <p class="text-white-75 font-weight-light mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aliquid culpa placeat explicabo iusto .</p>
+          <h4 class="text-white-75 font-weight-light mb-5">
+
+            <a href="tel:6136081121" class="phone_num"><i class="fas fa-phone mb-3"></i> (613) 608 1121</a>
+          </h4>
           <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Find Out More</a>
         </div>
       </div>
@@ -79,7 +83,7 @@
           <h2 class="text-white mt-0">We've got what you need!</h2>
           <hr class="divider light my-4">
           <p class="text-white-50 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptas accusamus, non illo voluptate adipisci sint error nam at a nisi ab blanditiis doloribus porro repudiandae dicta corrupti in culpa?</p>
-          <a class="btn btn-light btn-xl js-scroll-trigger" href="#services">Get Started!</a>
+          <a class="btn btn-light btn-xl js-scroll-trigger" data-toggle="modal" data-target="#modal" href="">Request a Free Estimate!</a>
         </div>
       </div>
     </div>
@@ -117,6 +121,16 @@
     </div>
   </section>
 
+
+
+  <!-- Call to Action Section -->
+  <section class="page-section bg-dark text-white">
+    <div class="container text-center">
+      <h2 class="mb-4">Get a Free Estimate Today!</h2>
+      <a class="btn btn-light btn-xl" href="" data-toggle="modal" data-target="#modal">Request an Estimate</a>
+    </div>
+  </section>
+
   <!-- Portfolio Section -->
   <section id="portfolio">
     <div class="container-fluid p-0">
@@ -134,6 +148,7 @@
             </div>
           </a>
         </div>
+
         <div class="col-lg-3 col-sm-6">
           <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
             <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
@@ -173,17 +188,49 @@
             </div>
           </a>
         </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+              <span style="font-size:100px">+</span>
+              </div>
+              <div class="project-name">
+
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+              <span style="font-size:100px">+</span>
+              </div>
+              <div class="project-name">
+
+              </div>
+            </div>
+          </a>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <a class="portfolio-box" href="img/portfolio/fullsize/7.jpg">
+            <img class="img-fluid" src="img/portfolio/thumbnails/7.jpg" alt="">
+            <div class="portfolio-box-caption">
+              <div class="project-category text-white-50">
+              <span style="font-size:100px">+</span>
+              </div>
+              <div class="project-name">
+
+              </div>
+            </div>
+          </a>
+        </div>
 
     </div>
   </section>
 
-  <!-- Call to Action Section -->
-  <section class="page-section bg-dark text-white">
-    <div class="container text-center">
-      <h2 class="mb-4">Get a Free Estimate Today!</h2>
-      <a class="btn btn-light btn-xl" href="">Request an Estimate</a>
-    </div>
-  </section>
 
   <!-- Contact Section -->
   <section class="page-section" id="contact">
@@ -206,8 +253,83 @@
           <a class="d-block" href="mailto:contact@capcityoffice.com">contact@capcityoffice.com</a>
         </div>
       </div>
+
     </div>
   </section>
+
+
+
+
+
+
+
+
+
+
+
+  <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Contact</h5>
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      
+      </div>
+      <div class="modal-body">
+        <form>
+
+
+          <div class="form-group">
+            <label for="full_name" class="col-form-label">Your Name:</label>
+            <input type="text" class="form-control" id="full_name">
+          </div>
+          <div class="form-group">
+            <label for="email" class="col-form-label">Email:</label>
+            <input type="text" class="form-control" id="email">
+          </div>
+          <div class="form-group">
+            <label for="Phone_num" class="col-form-label">Phone Number:</label>
+            <input type="text" class="form-control" id="Phone_num">
+          </div>
+          <div class="form-group">
+            <label for="how_did_you_hear_about_us" class="col-form-label">How did you hear about us?</label>
+            <input type="text" class="form-control" id="how_did_you_hear_about_us">
+          </div>
+
+          <div class="form-group">
+            <label for="message-text" class="col-form-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <a style="border-radius: 10rem;"class="btn btn-secondary" data-dismiss="modal" href="">Close</a>
+        <a style="border-radius: 10rem;"class="btn btn-primary" href="#">Send</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 
   <!-- Footer -->
   <footer class="bg-light py-5">
@@ -226,6 +348,10 @@
 
   <!-- Custom scripts for this template -->
   <script src="js/creative.min.js"></script>
+  <script>
+
+
+  </script>
 
 </body>
 
